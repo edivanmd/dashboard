3a6e65b3a9577bcf4ea4772243c240bd36658d7b
Edivan Melo <edivanmd@gmail.com>
2022-02-04 14:11:29 +1100

    sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Sidebar from "./components/Sidebar/Sidebar";
+import {BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
   return (
     <>
-      dashboard app
+      <Router>
+        <Sidebar />
+      </Router>
     </>
   );
 }
